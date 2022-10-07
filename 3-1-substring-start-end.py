ee9b20f4733ee14b8dc2b3b3198ad497fa39acfb
Husain Unwalla <mr.husainunwalla@gmail.com>
2022-10-06 21:08:43 -0700

    Hoemwork 3.1

---
+++ 3-1-substring-start-end.py
@@ -0,0 +1,19 @@
+import string
+from unittest import result
+
+
+'''
+Homework 3.1  COEN 279
+Using exhaustive search to count substrings starting and ending with "A"
+We check the starting and ending character of each possible substring.
+'''
+
+string = input('Enter your strings here: ')
+result = 0
+n = len(string)
+
+for i in range(n):
+    for j in range(i, n):
+        if string[i] == 'A' and string[j] == 'A':
+            result += 1
+print (result)
